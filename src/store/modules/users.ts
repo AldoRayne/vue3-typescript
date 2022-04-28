7165e1c11e34f0006fb7882d19568356aff9366f
Dmytro Konovaliuk <aldoray.n7@gmail.com>
2022-04-29 01:05:23 +0300

    styles

--- src/store/modules/users.ts
+++ src/store/modules/users.ts
@@ -14,17 +14,17 @@ export default {
     },
   },
   mutations: {
-    updateUsers(state: { value: Users[] }, payload: Users[]) {
+    getUsers(state: { value: Users[] }, payload: Users[]) {
       state.value = payload;
     },
   },
   actions: {
-    async updateUsers({ commit }: { commit: Commit }) {
+    async getUsers({ commit }: { commit: Commit }) {
       const { data } = await axios.get(
         "https://jsonplaceholder.typicode.com/users"
       );
 
-      commit("updateUsers", data);
+      commit("getUsers", data);
     },
   },
 };
--- src/views/JobDetails.vue
+++ src/views/JobDetails.vue
@@ -1,8 +1,23 @@
 <template>
-  <div>{{ users }}</div>
+  <div>
+    <transition-group tag="ul" class="user-list">
+      <li
+        v-for="(user, index) in users"
+        :key="user.id"
+        class="user-item"
+        :data-aos="aosParity(index) ? 'fade-right' : 'fade-left'"
+      >
+        <p class="user-name">{{ user.name }}</p>
+        <p class="user-email">{{ user.email }}</p>
+        <p class="user-address">{{ user.address.street }}</p>
+      </li>
+    </transition-group>
+  </div>
 </template>
 
 <script lang="ts">
+import Users from "@/types/users";
+
 import { defineComponent, onMounted, computed } from "vue";
 import { useStore } from "vuex";
 
@@ -13,16 +28,35 @@ export default defineComponent({
     const store = useStore();
 
     onMounted(() => {
-      store.dispatch("updateUsers");
+      store.dispatch("getUsers");
     });
 
-    const users = computed(() => {
+    const users = computed<Users[]>(() => {
       return store.getters.users;
     });
 
-    return { users };
+    const aosParity = (index: number): boolean => {
+      const isEven = !(index % 2);
+
+      return isEven;
+    };
+
+    return { users, aosParity };
   },
 });
 </script>
 
-<style scoped></style>
+<style scoped>
+.user-list {
+  max-width: 960px;
+  margin: 40px auto;
+  list-style-type: none;
+}
+.user-item {
+  padding: 16px;
+  background: #fff;
+}
+.user-item:not(:last-child) {
+  margin-bottom: 20px;
+}
+</style>
